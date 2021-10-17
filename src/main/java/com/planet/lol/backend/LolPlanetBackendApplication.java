6f6e9a7956e94b5609700b655cc73a9a9aafd570
Jaewook <eari100@naver.com>
2021-10-17 21:56:22 +0900

    first commit

---
+++ src/main/java/com/planet/lol/backend/LolPlanetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.planet.lol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolPlanetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolPlanetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/planet/lol/backend/LolPlanetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planet.lol.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolPlanetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
